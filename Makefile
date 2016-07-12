3e064e3a221d178e4a993af4390565cf8e7b80dc
Oleg Klimov <omgtech@gmail.com>
2016-07-13 00:44:53 +0300

    Makefile for viz

---
+++ Makefile
@@ -0,0 +1,92 @@
+UNAME := $(shell uname -s)
+OBJDIRR=.build-release
+OBJDIRD=.build-debug
+
+ifeq ($(UNAME),Linux)
+    PLATFORM=Linux
+    PKG  =pkg-config
+    MOC  =moc
+    CUDA_DIR := /usr/local/cuda
+    LIBS = -L/usr/lib64 -L$(CUDA_DIR)/lib64 -lhdf5_hl -lm -lopenblas
+    INC = -I/usr/include -I$(CUDA_DIR)/include
+    A =.a
+    LIB_PREFIX =lib
+endif
+
+ifeq ($(UNAME),Darwin)
+    PLATFORM=Darwin
+    PKG  =/usr/local/bin/pkg-config
+    MOC  =/usr/local/Cellar/qt/4.8.6/bin/moc
+    LIBS = -framework Accelerate
+    INC += -I/System/Library/Frameworks/Accelerate.framework/Versions/Current/Frameworks/vecLib.framework/Headers/
+endif
+
+CC=gcc
+LINK=gcc
+AR=ar r
+AR_OUT=
+LINK_OUT= -o
+LINK_OPT=
+MINUS_O = -o
+INC += -Iinclude
+CFLAGS   = -std=c++11 -Wall -DUSE_OPENCV -Wno-unused-variable -Wno-unused-function -fPIC -g -O3 $(INC)
+CFLAGSD  = -std=c++11 -Wall -DUSE_OPENCV -Wno-unused-variable -Wno-unused-function -fPIC -g -DDEBUG $(INC)
+LIBS    += -lstdc++ -llmdb -lboost_system-mt -lboost_thread-mt -lboost_filesystem -lboost_regex -ljsoncpp
+LIBS    += `$(PKG) --libs libglog`
+LIBSQT  += `$(PKG) --libs QtGui QtOpenGL`
+LIBSD    = $(LIBS)
+LIBSQTD  = $(LIBSQT)
+DEPENDS= -MMD -MF $@.dep
+
+EVERY_BIN=viz-r$(EXE) viz-d$(EXE)
+
+UTIL = lpr3/miniutils.cpp
+
+VIZ  = viz/viz.cpp
+
+UTIL_R = $(patsubst %.cpp, $(OBJDIRR)/%.o, $(UTIL))
+UTIL_D = $(patsubst %.cpp, $(OBJDIRD)/%.o, $(UTIL))
+VIZ_R = $(patsubst %.cpp, $(OBJDIRR)/%.o, $(VIZ))
+VIZ_D = $(patsubst %.cpp, $(OBJDIRD)/%.o, $(VIZ))
+
+EVERY_OBJ = $(VIZ_R) $(VIZ_D) $(UTIL_R) $(UTIL_D)
+DEP = $(patsubst %.o,%.o.dep, $(EVERY_OBJ))
+
+all: dirs $(EVERY_BIN)
+
+$(OBJDIRR)/viz/viz.o: viz/../.generated/viz.moc
+viz/../.generated/viz.moc: viz/viz.cpp
+	$(MOC) -o $@ $<
+
+viz-r$(EXE): $(VIZ_R) $(UTILS_R)
+	$(LINK) $(LINK_OPT) $(LINK_OUT)$@ $^ $(LIBS) $(LIBSQT)
+viz-d$(EXE): $(VIZ_D) $(UTILS_D)
+	$(LINK) $(LINK_OPT) $(LINK_OUT)$@ $^ $(LIBSD) $(LIBSQTD)
+
+$(OBJDIRR)/%.o: %.cpp
+	$(CC) $(CFLAGS) -c $<  $(MINUS_O)$@ $(DEPENDS)
+$(OBJDIRD)/%.o: %.cpp
+	$(CC) $(CFLAGSD) -c $<  $(MINUS_O)$@ $(DEPENDS)
+
+.PHONY: depend clean dirs
+
+clean:
+	$(RM) $(EVERY_BIN) $(EVERY_OBJ) .generated/*.moc *.ilk *.pdb $(DEP)
+	rm -rf .generated
+	rm -rf $(OBJDIRD)
+	rm -rf $(OBJDIRR)
+
+depends:
+	cat  $(DEP) > Makefile.dep
+
+# build directories
+.generated:
+	mkdir -p .generated
+$(OBJDIRR)/viz:
+	mkdir -p $@
+$(OBJDIRD)/viz:
+	mkdir -p $@
+
+dirs: .generated $(OBJDIRR)/viz $(OBJDIRD)/viz
+
+-include Makefile.dep
---
+++ viz/miniutils.cpp
@@ -0,0 +1,162 @@
+#include "../include/miniutils.h"
+#include <stdarg.h>
+#include <string>
+#include <boost/thread/thread.hpp>
+#include <boost/lexical_cast.hpp>
+
+#ifdef WIN32
+#include <Windows.h>
+#endif
+
+namespace miniutils {
+
+std::string stdprintf(const char* fmt, ...)
+{
+	char buf[32768];
+	va_list ap;
+	va_start(ap, fmt);
+	vsnprintf(buf, sizeof(buf), fmt, ap);
+	va_end(ap);
+	buf[32768-1] = 0;
+	return buf;
+}
+
+uint64_t now()
+{
+	boost::xtime xt;
+	boost::xtime_get(&xt, boost::TIME_UTC_);
+	uint64_t t = uint64_t(xt.sec)*1000000 + xt.nsec/1000;
+	return t;
+}
+
+FILE* open_or_die(const std::string& filename, const char* mode)
+{
+	FILE* f = fopen( filename.c_str(), mode );
+	if (!f)
+		throw std::runtime_error("cannot open '" + filename + "' with mode '" + mode + "': " + strerror(errno) );
+	return f;
+}
+
+void write_or_die(FILE* f, const void* data, uint32_t len, const std::string& errgen)
+{
+	assert(len);
+	int r = (int) fwrite(data, len, 1, f);
+	if (r!=1)
+		throw std::runtime_error("cannot write into '" + errgen + "': " + strerror(errno) );
+}
+
+void read_or_die(FILE* f, void* data, uint32_t len, const std::string& errgen)
+{
+	assert(len);
+	int r = (int) fread(data, len, 1, f);
+	if (r==0)
+		throw std::runtime_error("cannot read from '" + errgen + "', eof");
+	if (r!=1)
+		throw std::runtime_error("cannot read from '" + errgen + "': " + strerror(errno) );
+}
+
+std::string read_file(const std::string& name)
+{
+	FILE* f = open_or_die(name, "rb");
+	off_t ret = fseek(f, 0, SEEK_END);
+	if (ret==(off_t)-1)
+		throw std::runtime_error("cannot stat '" + name + "': " + strerror(errno) );
+	uint32_t file_size = (uint32_t) ftell(f); // ftell returns long int
+	fseek(f, 0, SEEK_SET);
+	std::string r;
+	if (file_size==0) return r;
+	r.resize(file_size);
+	try {
+		read_or_die(f, &r[0], file_size, name);
+		fclose(f);
+	} catch (...) {
+		fclose(f);
+		throw;
+	}
+	return r;
+}
+
+#ifdef WIN32
+#define MS_VC_EXCEPTION 0x406d1388
+typedef struct tagTHREADNAME_INFO {
+	DWORD dwType; // must be 0x1000
+	LPCSTR szName; // pointer to name (in same addr space)
+	DWORD dwThreadID; // thread ID (-1 caller thread)
+	DWORD dwFlags; // reserved for future use, most be zero
+} THREADNAME_INFO;
+
+void SetThreadName(DWORD dwThreadID, LPCTSTR szThreadName)
+{
+	THREADNAME_INFO info; info.dwType = 0x1000;
+	info.szName = szThreadName;
+	info.dwThreadID = dwThreadID; info.dwFlags = 0;
+	__try { RaiseException(MS_VC_EXCEPTION, 0, sizeof(info) / sizeof(DWORD), (ULONG_PTR*)&info); }
+	__except (EXCEPTION_CONTINUE_EXECUTION) {
+	}
+}
+
+void set_thread_name(const char* name)
+{
+	SetThreadName(-1, name);
+}
+
+#else
+#ifdef __linux__
+#include <sys/prctl.h>
+void set_thread_name(const char* name)
+{
+	prctl(PR_SET_NAME, name, 0, 0, 0);
+}
+
+#else
+#ifdef __APPLE__
+#include <pthread.h>
+void set_thread_name(const char* name)
+{
+	int r = pthread_setname_np(name);
+	if (r)
+		printf("Failed to set thread name, %s\n", strerror(r));
+}
+
+#else
+void set_thread_name(const char* name)
+{
+	#error "Your platform or compiler is not supported"
+}
+#endif
+#endif
+#endif
+
+void FullyIntelligentProfessionalConfigParser::from_string(const std::string& s) throw (std::runtime_error)
+{
+	std::istringstream ss(s);
+	for (int line=1; !ss.eof(); line++) {
+		char buf[1024], key[1024];
+		ss.getline(buf, sizeof(buf));
+		if (!buf[0] || buf[0]=='#') continue;
+		buf[1023] = 0;
+		int r = sscanf(buf, "%s", key);
+		if (r==1) {
+			char* val = buf + strlen(key);
+			while (*val && (*val==' ' || *val=='\t')) val++;
+			if (strings.find(key) != strings.end())
+				*strings[key] = val;
+			else if (ints.find(key) != ints.end())
+				*ints[key] = boost::lexical_cast<int>(val);
+			else if (reals.find(key) != reals.end())
+				*reals[key] = boost::lexical_cast<double>(val);
+			else if (callbacks.find(key) != callbacks.end())
+				callbacks[key](this, val);
+			else
+				throw std::runtime_error(
+					"unknown parameter '" + std::string(key) +
+					"', line " + boost::lexical_cast<std::string>(line) );
+		} else {
+			throw std::runtime_error(
+				"cannot interpret line " + boost::lexical_cast<std::string>(line)
+				);
+		}
+	}
+}
+
+} // namespace
