d533f482c8d3e77caa3ea1539993e3a5048dc1df
Oleg Klimov <omgtech@gmail.com>
2016-07-16 14:40:25 +0300

    Able to generate synthetic cliff demo

--- demo_synthetic_cliff.py
+++ demo_synthetic_cliff.py
@@ -1,11 +1,17 @@
+import os
+import controller.xp as xp
 import numpy as np
+
+dir = ".Cliff-v0"
+try: os.mkdir(dir)
+except: pass
 
 def synthetic_cliff():
     for c in range(15):
-        alpha      = 2*np.pi / 15 * c
+        alpha = 2*np.pi / 15 * c
         s = None
         for t in range(18):
-            sn = np.array( [100 * math.sin(alpha) * t/6, 100 * math.cos(alpha) * t/6] )
+            sn = np.array( [100 * np.sin(alpha) * t/6, 100 * np.cos(alpha) * t/6] )
             a  = np.array( [0] )
             r  = 0
             if t==17:
@@ -17,8 +23,8 @@ def synthetic_cliff():
     for c in range(15):
         alpha      = 2*np.pi / 15 * c
         alpha_next = 2*np.pi / 15 * (c+1)
-        side_s  = np.array( [100 * math.sin(alpha     ) * 5/6, 100 * math.cos(alpha     ) * 5/6] )
-        side_sn = np.array( [100 * math.sin(alpha_next) * 5/6, 100 * math.cos(alpha_next) * 5/6] )
+        side_s  = np.array( [100 * np.sin(alpha     ) * 5/6, 100 * np.cos(alpha     ) * 5/6] )
+        side_sn = np.array( [100 * np.sin(alpha_next) * 5/6, 100 * np.cos(alpha_next) * 5/6] )
         a = np.array( [0] )
         r = 0
         xp.replay.append( xp.XPoint(side_s,a,r,side_sn) )
@@ -28,8 +34,8 @@ def synthetic_cliff():
     for c in range(300):
         alpha = np.random.uniform(2*np.pi)
         beta  = np.random.uniform(2*np.pi)
-        side_s  = np.array( [100*math.sin(alpha), 100*math.cos(alpha)] )
-        side_sn = np.array( [side_s[0] + 5*math.sin(beta), side_s[1] + 5*math.cos(beta)] )
+        side_s  = np.array( [100*np.sin(alpha), 100*np.cos(alpha)] )
+        side_sn = np.array( [side_s[0] + 5*np.sin(beta), side_s[1] + 5*np.cos(beta)] )
         a = np.array( [0] )
         r = 0
         xp.replay.append( xp.XPoint(side_s,a,r,side_sn) )
@@ -37,3 +43,8 @@ def synthetic_cliff():
         xp.replay.append( xp.XPoint(side_sn,a,r,side_s) )
         xp.replay[-1].terminal = False
 
+
+xp.STATE_DIM = 2
+xp.ACTION_DIM = 1
+synthetic_cliff()
+xp.save(dir + "/cliff.json")
